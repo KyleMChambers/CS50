1b3f05ae1bf6d3670704865afeaddaf851f37b87
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2019-11-21 19:04:55 -0500

    'test'

---
+++ hello.py
@@ -0,0 +1,21 @@
+print('hello world')
+i = 21
+print(f"i is {i}")
+
+if x < 0:
+    print('x is blah')
+elif x > 0:
+    print('x is elif')
+else:
+    print('x is nah')
+
+name = 'alive'
+coordinates = (10,2,3,5,7)
+name = ['alice', 'barbara', 'ugly']
+
+s = set()
+s.add(1)
+s.add(21)
+s.add(13)
+s.add(11)
+s.add(7)
