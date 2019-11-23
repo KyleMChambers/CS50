c32dbd0c4edf5f12aa7b082695e403d632739299
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2019-11-23 17:27:09 -0500

    creating one less ta

--- lecture3/main.py
+++ lecture3/main.py
@@ -8,12 +8,6 @@ def home():
     return "Hello, World!"
 
 #creates a route or new pages with a slash
-@app.route("/donjuan")
-def don():
-        return "donjuan"
-if __name__ == "__main__":
-    app.run(debug=True)
-
 @app.route("/myfriendkmc")
 def don():
         return "myfriendkmc"
