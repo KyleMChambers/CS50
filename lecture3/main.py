572e2e6c19687351cf2c231fb7297ae974bda5a0
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2019-11-23 17:42:20 -0500

    added templates dir

--- lecture3/main.py
+++ lecture3/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template(index.html)
+    return render_template("index.html")
 #creates a route or new pages with a slash
 #you can use HTML5
 # you can reference an html or css file as long as it is in the same path
