6506b552b925ec4c8b19619f6876a15d2dc4f029
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2019-11-23 17:39:30 -0500

    index

--- lecture3/main.py
+++ lecture3/main.py
@@ -1,15 +1,16 @@
 #test app
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World!"
-
+def index():
+    return render_template(index.html)
 #creates a route or new pages with a slash
+#you can use HTML5
+# you can reference an html or css file as long as it is in the same path
 @app.route("/myfriendkmc")
 def don():
-        return "myfriendkmc"
+        return "<h1>myfriendkmc</h1>"
 if __name__ == "__main__":
     app.run(debug=True)
