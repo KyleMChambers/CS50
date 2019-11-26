b76dd6663e24c60473cc1661b702ece20f36cef0
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2019-11-25 19:37:14 -0500

    added the birthday lab

--- lecture3/main.py
+++ lecture3/main.py
@@ -1,11 +1,16 @@
 #test app
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    headline = "Hello Ronald"
+    now = datetime.datetime.now()
+    birthday = now.month == 8 and now.day == 18
+    birthday = True
+    return render_template("index.html", birthday=birthday)
 #creates a route or new pages with a slash
 #you can use HTML5
 # you can reference an html or css file as long as it is in the same path
